b254bcdff6f0de149a907a86792d0f3fa31c7e3b
Jonatandree <javasqueza@unah.hn>
2024-12-11 19:56:10 -0600

    cambio de Domian

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,7 +1,8 @@
 //src/services/settings
 const settings = {
-    domain: "https://api-nodo-dev.azurewebsites.net/api",
-    local: "http://localhost:8000/api"
+    //domain: "https://api-nodo-dev.azurewebsites.net/api",
+    //local: "http://localhost:8000/api"
+    local: "https://api-nodo-dev.azurewebsites.net/api"
 };
 
 export default settings;
